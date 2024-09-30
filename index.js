586c6fd033fdd7de1e0710ed44e5bb87d6a2f157
meshackkiplimo <meshackkimaiyo5@gmail.com>
2024-09-30 21:18:56 +0300

    crud operations

--- index.js
+++ index.js
@@ -12,6 +12,23 @@ const port = 4500;
 app.get("/", (req, res) => {
     res.send("This is a web server using express js");
 });
+//CRUD operations
+app.get("/createTable", (req, res) => {
+    const sql = `CREATE TABLE IF NOT EXISTS users (
+    id INT AUTO_INCREMENT PRIMARY KEY, 
+    name VARCHAR(255),
+     email VARCHAR(255))`;
+    
+    db.query(sql, (err) => {
+        if (err) {
+            console.log("error connection")
+            return res.status(500).send("error connection")
+        }
+        res.send("table created")
+        
+        
+    });
+    });
 
 //launch the server
 app.listen(port, () => {
